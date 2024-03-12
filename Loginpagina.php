828302ec26b89696d9c66d9e623e44b73dabb75c
Halinka <aleksandra.lepczynska@o2.pl>
2024-03-12 16:03:28 +0100

    Update Loginpagina.php
    Inlogformulier is aangemaakt zonder css en php

---
+++ Loginpagina.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inloggen</title>
+</head>
+<body>
+        <form class="" method="post">
+        <label for="gebruikersnaam">Gebruikersnaam</label>
+        <input type="text" id="gebruikersnaam" name="gebruikersnaam" required>
+
+        <label for="wachtwoord">Wachtwoord</label>
+        <input type="password" id="wachtwoord" name="wachtwoord" required>
+
+        <input type="submit" name="submit" value="Inloggen">
+</body>
+</html>
+<form>
