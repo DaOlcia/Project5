0bda15192f7afb7a6f875c550db61169139d970e
Gaby <137078206+GvGurp@users.noreply.github.com>
2024-03-12 14:01:19 +0100

    Registratie formulier
    Registratie formulier is aangemaakt, maar zonder 'action' en 'css'

---
+++ Registratiepagina.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registreren</title>
+</head>
+<body>
+    <h1> Registratie formulier </h1> 
+
+    <form class="" action="" method="post"> 
+        <label for="voornaam">Voornaam</label>
+        <input type="text" id="voornaam" name="voornaam" required>
+
+        <label for="achternaam">Achternaam</label>
+        <input type="text" id="achternaam" name="achternaam" required>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="telefoonnummer">Telefoonnummer</label>
+        <input type="tel" id="telefoonnummer" name="telefoonnummer" required>
+
+        <label for="gebruikersnaam">Gebruikersnaam</label>
+        <input type="text" id="gebruikersnaam" name="gebruikersnaam" required>
+
+        <label for="wachtwoord">Wachtwoord</label>
+        <input type="password" id="wachtwoord" name="wachtwoord" required>
+
+        <input type="submit" name="submit" value="Registreren">
+
+    </form> 
+</body>
+</html>
