bc18bca2f3e4654789a27548055acea776230ae0
Gaby <137078206+GvGurp@users.noreply.github.com>
2024-03-15 15:15:00 +0100

    Navigatie
    Navigatie gemaakt

--- Boekenpagina.php
+++ Boekenpagina.php
@@ -5,13 +5,13 @@
     <script src="https://kit.fontawesome.com/ecd9dddd0d.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="HoofdCss.css">
-    <title>Hoofdpagina</title>
+    <title>Boeken pagina</title>
 </head>
 <body>
 
 <nav>
-    <ul> <i class="fa-solid fa-star"></i> <li><a href="Hoofdpagina.php">Hoofdpagina</li> </ul> 
-    <ul> <li><a href="Boekenpagina.php">Boeken</li> </ul> 
+    <ul> <li><a href="Hoofdpagina.php">Hoofdpagina</li> </ul> 
+    <ul> <i class="fa-solid fa-star"></i> <li><a href="Boekenpagina.php">Boeken</li> </ul> 
     <ul> <li><a href="Loginpagina.php">Login</li> </ul> 
     <ul> <li><a href="Registratiepagina.php">Registreer</li></ul> 
 </nav> 
--- Registratiepagina.php
+++ Registratiepagina.php
@@ -2,11 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/ecd9dddd0d.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registreren</title>
-    <link rel="stylesheet" href="hoofdCss.css">
+    <link rel="stylesheet" href="HoofdCss.css">
 </head>
 <body>
+<nav>
+    <ul> <li><a href="Hoofdpagina.php">Hoofdpagina</li> </ul> 
+    <ul> <li><a href="Boekenpagina.php">Boeken</li> </ul> 
+    <ul> <li><a href="Loginpagina.php">Login</li> </ul> 
+    <ul> <i class="fa-solid fa-star"></i> <li><a href="Registratiepagina.php">Registreer</li></ul> 
+</nav> 
+
     <h1> Registratie formulier </h1> 
 
     <form class="formulier" action="Databse/Registratie.php" method="post"> 
